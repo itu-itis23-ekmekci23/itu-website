7a6ef78a05931d26740346e65b1d4642c60022c9
itu-itis-ekmekci23 <ekmekci23@itu.edu.tr>
2024-07-14 03:23:41 +0300

    logos are being added

--- src/card.js
+++ src/card.js
@@ -4,9 +4,10 @@ import './card.css';
 function Card({ prop }) {
     return (
         <div className="card">
+            <img src="sahibinden.png" alt={`${prop} logo`} />
             <p>Bu Card olacak. ({prop})</p>
         </div>
     )
 }
 
-export default Card
+export default Card;
