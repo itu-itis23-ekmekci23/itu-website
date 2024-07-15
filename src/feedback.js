9c84345047605be0e66e801de4862bafdb8af2c2
itu-itis-ekmekci23 <ekmekci23@itu.edu.tr>
2024-07-16 00:55:50 +0300

    frontend of feedback system is completed

--- src/feedback.js
+++ src/feedback.js
@@ -1,9 +1,12 @@
 import React from "react";
+import './feedback.css'
 
 function Feedback() {
     return (
-        <div>
-            <p>Bu Feedback olacak.</p>
+        <div className="feedbackdiv">
+            <p>Şikayet ve önerilerinizi buradan bize bildirebilirsiniz:</p>
+            <input id="emailinput" type="email" placeholder="İTÜ e-mail adresi" /><br />
+            <textarea id="messageinput" type="text" placeholder="Mesajınız" />
         </div>
     )
 }
