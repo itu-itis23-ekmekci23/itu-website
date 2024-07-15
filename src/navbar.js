5c2d5f62dcf2c096b0e7c1a033d824e0becbc8ba
itu-itis-ekmekci23 <ekmekci23@itu.edu.tr>
2024-07-16 00:54:21 +0300

    small fixes to navbar

--- src/navbar.js
+++ src/navbar.js
@@ -5,11 +5,11 @@ function Navbar() {
     return (
         <div className="bigdiv">
             <span> ITU WEBSITE</span>
-            <a href="google.com" className="dib grow">Sahibinden</a>
-            <a href="google.com" className="dib grow">Süreli İlanlar</a>
-            <a href="google.com" className="dib grow">Ürün Kiralama</a>
-            <a href="google.com" className="dib grow">Ders Yardımcısı</a>
-            <a href="google.com" className="dib grow">Harf Notu Hesaplama</a>
+            <a href="https://www.google.com" id="nv1" className="dib grow">Sahibinden</a>
+            <a href="https://www.google.com" id="nv2" className="dib grow">Süreli İlanlar</a>
+            <a href="https://www.google.com" id="nv3" className="dib grow">Ürün Kiralama</a>
+            <a href="https://www.google.com" id="nv4" className="dib grow">Ders Yardımcısı</a>
+            <a href="https://www.google.com" id="nv5" className="dib grow">Harf Notu Hesaplama</a>
         </div>
     )
 }
